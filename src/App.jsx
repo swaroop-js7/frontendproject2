663b74fa6457ade46fc60f38cccf1ebb723bc064
swaroop-js7 <jyothiswaroop2898@gmail.com>
2025-07-30 15:28:33 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import {Link}from'react-router-dom';
-
-
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Welcome to Frontend class KLU</h1>
